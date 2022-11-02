11e2d92998aaf06c9707d4eadb396c00cc615472
Andrew Spott <andrew.spott@gmail.com>
2022-11-01 21:43:05 -0600

    deleted old files

--- spacemacs/.spacemacs_layers/pio/funcs.el
+++
@@ -1,6 +0,0 @@
-;; (defun pio/platformio-conditionally-enable ()
-;;   "Enable `platformio-mode' only when a `platformio.ini' file is present in project root."
-;;   (condition-case nil
-;;       (when (projectile-verify-file "platformio.ini")
-;;         (platformio-mode 1))
-;;     (error nil)))
