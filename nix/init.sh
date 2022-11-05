28f6d0928273acb880b82c24db6f38a5ea80d4b8
Andrew Spott <andrew.spott@gmail.com>
2022-11-04 21:18:08 -0600

    merge

--- nix/init.sh
+++ nix/init.sh
@@ -1,10 +1,13 @@
 curl -L https://nixos.org/nix/install | sh
 
 nix build --no-link .\#homeConfigurations.spott.activationPackage
-$(nix path-info .\#homeConfigurations.spott.activationPackage)"/activate
+"$(nix path-info .\#homeConfigurations.spott.activationPackage)"/activate
 
 # after install:
 # home-manager switch --flake "path:.#spott"
-sudo softwareupdate --install-rosetta --agree-to-license # allow running x86_64 code
-
-
+#
+# allow running x86_64 code
+#sudo softwareupdate --install-rosetta --agree-to-license 
+#
+# to install paq on nvim
+# nvim --headless -u NONE -c 'lua require("bootstrap").bootstrap_paq()'
