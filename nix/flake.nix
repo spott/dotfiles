3341a2b1ae6ddaab21e22f1be985ed7f1ef11878
Andrew Spott <andrew.spott@gmail.com>
2022-11-17 13:15:37 -0700

    home-manager multiple systems works?

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,46 +9,49 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     
-    flake-utils = {
+    /* flake-utils = {
       url = "github:numtide/flake-utils";
-    };
+    }; */
   };
 
-  outputs = { nixpkgs, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-darwin ] (system: 
+  outputs = { nixpkgs, home-manager, ... }:
+    #flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-darwin ] (system: 
     let 
       #system = "aarch64-darwin";
       #system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system;
+      pkgs = sys: import nixpkgs { system = sys;
                               config = {allowUnfree = true;};
                               };
     in {
-      homeConfigurations."spott@Normandy.local" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs system;
-        modules = [
-          ./normandy.nix
-          ./common.nix
-          ./darwin-common.nix
-          ./zsh/zsh.nix
-        ];
+        packages = {aarch64-darwin.homeConfigurations = {
+        "spott@Normandy.local" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgs "aarch64-darwin";
+          modules = [
+            ./normandy.nix
+            ./common.nix
+            ./darwin-common.nix
+            ./zsh/zsh.nix
+          ];
+        };
+        "spott@Endeavor.local" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgs "aarch64-darwin";
+          modules = [
+            ./endeavor.nix
+            ./common.nix
+            ./darwin-common.nix
+            ./zsh/zsh.nix
+          ];
+        };};
+        x86-64-linux.homeConfigurations = {
+        "spott@devbox" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgs "x86-64-linux";
+          modules = [
+            ./devbox.nix
+            ./common.nix
+            ./zsh/zsh.nix
+          ];
+        };
       };
-      homeConfigurations."spott@Endeavor.local" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs system;
-        modules = [
-          ./endeavor.nix
-          ./common.nix
-          ./darwin-common.nix
-          ./zsh/zsh.nix
-        ];
       };
-      homeConfigurations."spott@devbox" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs system;
-        modules = [
-          ./devbox.nix
-          ./common.nix
-          ./zsh/zsh.nix
-        ];
-      };
-    }
-  );
+    };
 }
